50f8b20cc1caa879906e2e6ea553bc9c681b3bc2
Adam Sisa <53064412+sisadm@users.noreply.github.com>
2021-03-27 22:22:30 +0000

    Game class
    Create constructor, and two function for class

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,25 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Game.js */
+class Game {
+    constructor() {
+        this.missed = 0;
+        this.phrase = ['I am never at home on Sundays',
+                        'The river stole the gods',
+                        'Love is not like pizza',
+                        'I’m a living furnace',
+                        'So long and thanks for the fish',
+                        'The book is in front of the table'];
+        this.activePhrase = null;
+    }
+
+    startGame(){
+
+    }
+
+    getRandomPhrase() {
+        return this.phrase[Math.floor(Math.random()* this.phrase.length)];
+    }
+}
+
+const a = new Game();
+
+a.getRandomPhrase();
+
