7d42be7e7d1ad9345796239a5ea0a2e34a3dea16
Adam Sisa <53064412+sisadm@users.noreply.github.com>
2021-04-05 20:32:59 +0100

    App.Js
    Event Listener for overlay which create a new Game class. If you restart the Game it clear the hearts, clicked buttons and phrase. Add a new phrase into the game.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,46 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * app.js */
+const overlay = document.querySelector('#overlay');
+const qwerty = document.querySelector('#qwerty');
+let newGame;
+
+
+overlay.addEventListener('click', (e) => {
+    if(e.target.tagName == 'BUTTON') {
+
+        if(e.target.classList.contains('reset-game')){
+            const phraseLi = document.querySelectorAll('#phrase li');
+            const hearthImg = document.querySelectorAll('#scoreboard img');
+            const qwertyBtn = document.querySelectorAll('#qwerty button');
+
+            // remove class which added when game is end
+            e.target.classList.remove('reset-game');
+
+            // clear old phrase Li
+            for(let li of phraseLi) {
+                li.remove();
+            }
+
+            // change hearth img src back to normal
+            for(let img of hearthImg) {
+                img.src = 'images/liveHeart.png';
+            }
+
+            // remove the additionals btn classes 
+            for(let btn of qwertyBtn) {
+                btn.classList.remove('chosen') ||
+                btn.classList.remove('wrong');
+            }
+
+        }
+
+        if(e.target.textContent == 'Start Game') {
+            newGame = new Game();
+            newGame.startGame();
+        }
+    }
+})
+
+qwerty.addEventListener('click', (e) => {
+    if(e.target.classList == 'key') {
+        newGame.handleInteraction(e.target);
+    }
+})
