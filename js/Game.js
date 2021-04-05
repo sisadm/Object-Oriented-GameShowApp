e1568598c76a3cf7a7c7a0b2e9625b576092b66a
Adam Sisa <53064412+sisadm@users.noreply.github.com>
2021-04-05 20:26:53 +0100

    Game.Js
    create HandleInteraction - which find when the user click a button and its match the letter from the Phrase.
    
    Removelife - if you miss a letter you lose a life
    
    checkForWin- check the phrase length is the same as the right chosen
    
    gameOver - when the game is end depends the end

--- js/Game.js
+++ js/Game.js
@@ -1,12 +1,12 @@
 class Game {
     constructor() {
         this.missed = 0;
-        this.phrase = ['I am never at home on Sundays',
-                        'The river stole the gods',
-                        'Love is not like pizza',
-                        'I’m a living furnace',
-                        'So long and thanks for the fish',
-                        'The book is in front of the table'];
+        this.phrase = ['i am never at home on Sundays',
+                        'the river stole the gods',
+                        'love is not like pizza',
+                        'i am a living furnace',
+                        'so long and thanks for the fish',
+                        'the book is in front of the table'];
         this.activePhrase = null;
     }
 
@@ -14,16 +14,83 @@ class Game {
     startGame(){
         document.querySelector('#overlay').style.display = 'none';
         this.activePhrase = this.getRandomPhrase();
-        const newGame = new Phrase(this.activePhrase);
-        newGame.addPhraseToDisplay();
+        let newPhrase = new Phrase(this.activePhrase);
+        newPhrase.addPhraseToDisplay();
     }
 
+    // get random phrase
     getRandomPhrase() {
         return this.phrase[Math.floor(Math.random()* this.phrase.length)];
     }
+
+    // buttons interaction with the random Phrase
+    handleInteraction(target){
+        // const phrase = Array.from(document.querySelectorAll('#phrase li'));
+        // const phraseLetters = phrase.map(i => i.textContent); // separete text from the Li
+        const NewPheaseClass = new Phrase (this.activePhrase);
+        const arrayPhrase = this.activePhrase.split('');
+        const liPhrase = document.querySelectorAll('#phrase li');
+        
+                
+        if(this.activePhrase.includes(target.textContent)) { // if true 
+            target.classList.add('chosen');
+            NewPheaseClass.checkLetter(target.textContent);
+            this.checkForWin()
+        } else {
+            target.classList.add('wrong');
+            this.removeLife()
+        }                
+            
+        
+
+
+        // qwerty.addEventListener('click', (e) => {
+        //     if(e.target.tagName == 'BUTTON'){
+        //         if(phraseLetters.includes(e.target.textContent)) { // if true 
+        //             e.target.classList.add('chosen');
+        //             NewPheaseClass.checkLetter(e.target.textContent);
+        //             this.checkForWin()
+        //         } else {
+        //             e.target.classList.add('wrong');
+        //             this.removeLife()
+        //         }
+                
+        //     }
+        // })
+    }
+
+    removeLife() {
+        const hearth = document.querySelectorAll('#scoreboard li img');
+        if(this.missed == 4) {
+            this.gameOver('loss');
+        } else {
+            hearth[this.missed].src = 'images/lostHeart.png';
+            this.missed += 1;
+        }
+    }
+
+    checkForWin(){
+        const letterClass = document.querySelectorAll('.letter').length;
+        const showClass = document.querySelectorAll('.show').length;
+        if(letterClass == showClass) {
+            this.gameOver('win');
+        }
+    }
+
+    gameOver(msg) {
+        const overLay = document.querySelector('#overlay');
+        const h1 = document.querySelector('#game-over-message');
+        const startBtn = document.querySelector('#overlay button');
+        overLay.style.display = 'block'
+        if(msg == 'win') {
+            overLay.classList.add('win');
+            h1.innerHTML = 'Awesome, you are win. Greate Job!'; 
+        } else {
+            overLay.classList.add('loss');
+            h1.innerHTML = "Oh no, you lost. Let's try it again."; 
+        }
+
+        startBtn.classList.add('reset-game');
+    }
 }
-
-const a = new Game();
-
-a.startGame();
 
